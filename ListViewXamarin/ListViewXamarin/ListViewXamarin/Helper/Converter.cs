2f5287aab556130865267fdc7255df40713761bb
Jayaleshwari N <jayaleshwarin@syncfusion.com>
2021-01-12 17:53:32 +0530

    XAMARIN-34565 - Sample and screenshot added.

---
+++ ListViewXamarin/ListViewXamarin/ListViewXamarin/Helper/Converter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ListViewXamarin
+{
+    public class SwipingBoolToImageConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if ((bool)value)
+                return ImageSource.FromResource("ListViewXamarin.Images.Favorites.png");
+            else
+                return ImageSource.FromResource("ListViewXamarin.Images.InboxIcon.png");
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ListViewXamarin/ListViewXamarin/ListViewXamarin/ViewModel/ListViewSwipingViewModel.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ListViewXamarin
+{
+    public class ListViewSwipingViewModel
+    {
+        #region Fields
+
+        private ObservableCollection<ListViewInboxInfo> inboxInfo;
+        #endregion
+
+        #region Constructor
+
+        public ListViewSwipingViewModel()
+        {
+            SetFavoriteCommand = new Command<object>(SetFavorites);
+            GenerateSource();
+        }
+        #endregion
+
+        #region Properties
+
+        public ObservableCollection<ListViewInboxInfo> InboxInfo
+        {
+            get { return inboxInfo; }
+            set { this.inboxInfo = value; }
+        }
+
+        public Command<object> SetFavoriteCommand { get; set; }
+        #endregion
+
+        #region Methods
+
+        private void GenerateSource()
+        {
+            ListViewInboxInfoRepository inboxinfo = new ListViewInboxInfoRepository();
+            inboxInfo = inboxinfo.GetInboxInfo();
+        }
+
+        private void SetFavorites(object obj)
+        {
+            var customCommandParameter = obj as CustomCommandParameter;
+            (customCommandParameter.CommandParameter as ListViewInboxInfo).IsFavorite = !(customCommandParameter.CommandParameter as ListViewInboxInfo).IsFavorite;
+            customCommandParameter.ListView.ResetSwipe();
+        }
+        #endregion
+    }
+}
